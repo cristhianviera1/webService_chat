96f73c3987fffc77100181298259f733b867d017
jac.aguirre <jac.aguirre@yavirac.edu.ec>
2020-02-19 22:53:45 -0500

    ejemplo para guardar imagen

--- controllers/novedadesController.js
+++ controllers/novedadesController.js
@@ -7,21 +7,6 @@ const path = require('path');
 
 var urlimage = null;
 
-//Subir imagenes
-router.post('/images', upload.single('image'), async function(req, res) {
-    console.log("Este es el buffer",req.file.buffer);
-    const imagePath = path.join(__dirname, '../public/images/chat');
-    const fileUpload = new Resize(imagePath);
-
-    if(!req.file) {
-        res.status(401).json({error: 'Por favor proporciona una imagen'});
-    }
-
-    const filename = await fileUpload.save(req.file.buffer);
-    return res.status(200).json({url: "http://192.168.1.8:4000/images/chat"+filename});
-    
-})
-
 //-------------------------------------
 
 router.get('/', async (req, res) => {
