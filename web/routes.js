e59bbf7327a4486cc080ce8d863c15ed63bd3ff1
gabriel <cristhian.viera1@gmail.com>
2020-08-16 14:13:22 -0500

    Remove unused routes

--- web/routes.js
+++ web/routes.js
@@ -4,10 +4,7 @@
 */
 
 'use strict';
-
-const routeHandler = require('./../handlers/route-handler');
-global.__root   = __dirname + '/'; 
-let webroutes = require(__root + '../controllers/routesweb');
+global.__root = __dirname + '/';
 let userController = require(__root + '../controllers/userController');
 let newController = require(__root + '../controllers/newsController');
 let productController = require(__root + '../controllers/productController');
@@ -21,17 +18,6 @@ class Routes{
 
 	/* creating app Routes starts */
 	appRoutes(){
-		this.app.post('/usernameAvailable', routeHandler.userNameCheckHandler);
-
-		this.app.post('/register', routeHandler.registerRouteHandler);
-
-		this.app.post('/login', routeHandler.loginRouteHandler);
-
-		this.app.post('/userSessionCheck', routeHandler.userSessionCheckRouteHandler);
-
-		this.app.post('/getMessages', routeHandler.getMessagesRouteHandler);
-
-		this.app.use('/webroutes', webroutes);
 
 		this.app.use('/', userController);
 
@@ -40,8 +26,6 @@ class Routes{
 		this.app.use('/products', productController);
 
 		this.app.use('/form', formController);
-
-		this.app.get('*', routeHandler.routeNotFoundHandler);
 
 		
 	}
